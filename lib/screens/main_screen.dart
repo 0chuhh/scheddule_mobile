6b678ae2d2a9d2c9413cc9fc48a127d8f525e2d5
Strukov Vladislav <sivistrukov@gmail.com>
2023-02-27 16:26:30 +0900

    feat: стилизация нижнего меню навигации
    Добавлены скругленные края и тень, к иконкам и тексту заданы новые цвета.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:schedule_mobile/screens/bell_schedule_screen.dart';
 import 'package:schedule_mobile/screens/my_schedule_screen.dart';
-import 'package:schedule_mobile/screens/notifications_screen.dart';
+import 'package:schedule_mobile/screens/reminders_screen.dart';
 import 'package:schedule_mobile/screens/schedules_screen.dart';
 import 'package:schedule_mobile/screens/settings_screen.dart';
+import 'package:schedule_mobile/utils/styles.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -15,7 +16,7 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
   static final List<Widget> _screens = <Widget>[
-    const NotificationScreen(),
+    const RemindersScreen(),
     const BellScheduleScreen(),
     const MyScheduleScreen(),
     const SchedulesScreen(),
@@ -31,35 +32,56 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _screens[_selectedIndex],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: "notifications",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.timer),
-            label: "alarm schedule",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.schedule),
-            label: "my schedule",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.schedule),
-            label: "schedule",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "settings",
-          ),
+        body: Center(
+          child: _screens[_selectedIndex],
+        ),
+        bottomNavigationBar: bottomNavigationBar);
+  }
+
+  Widget get bottomNavigationBar {
+    return Container(
+      decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 5),
         ],
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+        child: BottomNavigationBar(
+          unselectedItemColor: Styles.textColor,
+          selectedItemColor: Styles.primaryColor,
+          unselectedFontSize: 12,
+          selectedFontSize: 12,
+          backgroundColor: Colors.white,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: "notifications",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.timer),
+              label: "alarm schedule",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.schedule),
+              label: "my schedule",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.schedule),
+              label: "schedule",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "settings",
+            ),
+          ],
+        ),
       ),
     );
   }
