0327a39665def7aa20df8db035bc5569d52fe4b0
Strukov Vladislav <sivistrukov@gmail.com>
2023-02-28 16:13:59 +0900

    feat: стилизация статус бара

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:schedule_mobile/screens/main_screen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarBrightness: Brightness.dark,
+    statusBarIconBrightness: Brightness.dark,
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const MainApp());
 }
 
