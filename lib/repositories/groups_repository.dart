059fdd2402f549a1d989c1742a676221c8b24849
Strukov Vladislav <sivistrukov@gmail.com>
2023-03-11 11:36:35 +0900

    feat: добавлен функционал для получения групп по API

---
+++ lib/repositories/groups_repository.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:schedule_mobile/models/group_model.dart';
+
+class GroupsRepository {
+  Future<List<GroupModel>> getGroups() async {
+    final response =
+        await Dio().get('https://zabgu.ru/modules/raspisanie/api/groups.php');
+    final data = jsonDecode(response.data) as List;
+    final groupsList = data
+        .map((e) => GroupModel(name: (e as Map<String, dynamic>)['group_name']))
+        .toList();
+    return groupsList;
+  }
+
+  Future<List<GroupModel>> getGroupsByName(String groupName) async {
+    final response = await Dio().get(
+      'https://zabgu.ru/modules/raspisanie/api/groups.php',
+      queryParameters: {'groupName': groupName}
+    );
+    final data = jsonDecode(response.data) as List;
+    final groupsList = data
+        .map((e) => GroupModel(name: (e as Map<String, dynamic>)['group_name']))
+        .toList();
+    return groupsList;
+  }
+}
